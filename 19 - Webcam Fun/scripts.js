80bca4377b0bbbb058ccb915c459f8f3d178e3cf
Sanja Wetzel <sanjwich@gmail.com>
2020-11-27 18:31:02 +0100

    added the rgbSplit filter and played with the globalAlpha. Music video realnesssss

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -27,7 +27,9 @@ function paintToCanvas() {
     //take pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
     //manipulate them
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
     //put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -49,8 +51,17 @@ function takePhoto() {
 function redEffect(pixels){
   for(let i = 0; i < pixels.data.length; i+=4) {
     pixels.data[i + 0] = pixels.data[i + 0] + 100; //r
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //b
+    pixels.data[i + 1] = pixels.data[i + 1] - 10; //g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //b
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //r
+    pixels.data[i + 500] = pixels.data[i + 1]; //g
+    pixels.data[i - 550] = pixels.data[i + 2]; //b
   }
   return pixels;
 }
