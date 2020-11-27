1b027cb21c3a82f57993657429b0d5b75023788c
Sanja Wetzel <sanjwich@gmail.com>
2020-11-27 18:08:05 +0100

    got canvas and take photo function running, now debugging the pixel manipulation part for the red effect filter

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -10,7 +10,51 @@ function getVideo() {
       console.log(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
+    })
+    .catch(err => {
+      console.error('Damn thats cold!!', err);
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    //take pixels out
+    // let pixels = ctx.getImageData(video, 0, 0, width, height);
+    //manipulate them
+    // pixels = redEffect(pixels);
+    //put them back
+    // ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  console.log(data);
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Gorgeous lady">`
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels){
+  for(let i = 0; i < pixels.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; //r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //b
+  }
+  return pixels;
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
